2d340d2e07387db988fc62d97caf120b24ba1ebd
AnkilP <ankil335@gmail.com>
2022-03-13 18:59:19 -0400

    updated server

---
+++ exchange.go
@@ -0,0 +1,160 @@
+package LegalRelief
+
+import (
+	"container/heap"
+	"errors"
+	"math/rand"
+)
+
+type stockItem struct {
+	averagePrice float64
+	buyOrders    PriorityQueue
+	sellOrders   PriorityQueue
+}
+
+type Exchange struct {
+	pq     map[string]*stockItem
+	prices map[string]float64
+}
+
+func (e *Exchange) describeStock(stock string) (float64, error) {
+	if val, ok := e.prices[stock]; ok {
+		return val, nil
+	}
+	return 0.0, errNotFound
+}
+
+func NewStockItem() *stockItem {
+	buyOrders := make(PriorityQueue, 0)
+	heap.Init(&buyOrders)
+	sellOrders := make(PriorityQueue, 0)
+	heap.Init(&sellOrders)
+	return &stockItem{
+		averagePrice: 0.0,
+		buyOrders:    buyOrders,
+		sellOrders:   sellOrders,
+	}
+}
+
+func (e *Exchange) newStockType(stock string) {
+	if _, ok := e.pq[stock]; !ok {
+		e.pq[stock] = NewStockItem()
+	}
+}
+
+var errNotFound = errors.New("could not find stock")
+
+func (e *Exchange) peekBuy(stock string) (*Item, error) {
+	if val, ok := e.pq[stock]; ok {
+		return heap.Pop(&val.buyOrders).(*Item), nil
+	}
+	return nil, errNotFound
+}
+
+func (e *Exchange) peekSell(stock string) (*Item, error) {
+	if val, ok := e.pq[stock]; ok {
+		return heap.Pop(&val.sellOrders).(*Item), nil
+	}
+	return nil, errNotFound
+}
+
+func (e *Exchange) addBuy(stock string, item *Item) error {
+	if val, ok := e.pq[stock]; ok {
+		heap.Push(&val.buyOrders, item)
+		return nil
+	}
+	return errNotFound
+}
+
+func (e *Exchange) addSell(stock string, item *Item) error {
+	if val, ok := e.pq[stock]; ok {
+		heap.Push(&val.sellOrders, item)
+		return nil
+	}
+	return errNotFound
+}
+
+type TransactionStatus string
+
+const (
+	SUCCESS TransactionStatus = "Success"
+	PENDING TransactionStatus = "Pending"
+	FAILURE TransactionStatus = "Failure"
+)
+
+func min(a, b uint64) uint64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func (e *Exchange) buy(stock string, price float64, volume uint64) (TransactionStatus, error) {
+	err := e.addBuy(stock, &Item{
+		id:     uint64(rand.Int()),
+		price:  price,
+		volume: volume,
+	})
+	if err != nil {
+		return FAILURE, err
+	}
+
+	err = e.matchOrders(stock)
+	if err != nil {
+		return FAILURE, err
+	}
+
+	// hack - we assume it got matched for demo purposes
+
+	return SUCCESS, nil
+}
+
+func (e *Exchange) matchOrders(stock string) error {
+	for {
+		sellOrder, err := e.peekSell(stock)
+		if err != nil {
+			return err
+		}
+
+		if sellOrder == nil {
+			return nil
+		}
+
+		buyOrder, err := e.peekBuy(stock)
+		if err != nil {
+			return err
+		}
+
+		if buyOrder == nil {
+			return nil
+		}
+
+		if buyOrder.price >= sellOrder.price {
+			amount := min(buyOrder.volume, sellOrder.volume)
+			sellOrder.volume -= amount
+			buyOrder.volume -= amount
+			e.prices[stock] = (buyOrder.price + sellOrder.price) / 2
+			if sellOrder.volume != 0 {
+				err := e.addSell(stock, sellOrder)
+				if err != nil {
+					return errNotFound
+				}
+			}
+
+			if buyOrder.volume != 0 {
+				err := e.addBuy(stock, buyOrder)
+				if err != nil {
+					return errNotFound
+				}
+			}
+		} else {
+			return nil
+		}
+	}
+}
+
+func NewExchange() *Exchange {
+	return &Exchange{
+		pq: make(map[string]*stockItem, 0),
+	}
+}
--- main.go
+++ main.go
@@ -1,92 +1,76 @@
 package LegalRelief
 
 import (
-	"container/heap"
-	"errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-// An Item is something we manage in a priority queue.
-type Item struct {
-	value    string // The value of the item; arbitrary.
-	priority int    // The priority of the item in the queue.
-	volume   uint64
-	// The index is needed by update and is maintained by the heap.Interface methods.
-	index int // The index of the item in the heap.
-}
-
-// A PriorityQueue implements heap.Interface and holds Items.
-type PriorityQueue []*Item
-
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
-	return pq[i].priority > pq[j].priority
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *PriorityQueue) Push(x interface{}) {
-	n := len(*pq)
-	item := x.(*Item)
-	item.index = n
-	*pq = append(*pq, item)
-}
-
-func (pq *PriorityQueue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil  // avoid memory leak
-	item.index = -1 // for safety
-	*pq = old[0 : n-1]
-	return item
-}
-
-// update modifies the priority and value of an Item in the queue.
-func (pq *PriorityQueue) update(item *Item, value string, priority int) {
-	item.value = value
-	item.priority = priority
-	heap.Fix(pq, item.index)
-}
-
-type Exchange struct {
-	pq     map[string]PriorityQueue
-	prices map[string]float64
-}
-
-var errNotFound = errors.New("could not find stock")
-
-func (e *Exchange) peek(stock string) (*Item, error) {
-	if val, ok := e.pq[stock]; ok {
-		return heap.Pop(&val).(*Item), nil
-	}
-	return nil, errNotFound
-}
-
-type TransactionStatus string
-
-const (
-	SUCCESS TransactionStatus = "Success"
-	PENDING TransactionStatus = "Pending"
-	FAILURE TransactionStatus = "Failure"
-)
-
-func (e *Exchange) buy(stock string, volume uint64) TransactionStatus {
-
-}
-
-func NewExchange() *Exchange {
-	return &Exchange{
-		pq: make(map[string]PriorityQueue, 0),
-	}
+type Order struct {
+	Price  float64 `json:"price"`
+	Volume uint64  `json:"volume"`
 }
 
 func main() {
 	e := NewExchange()
-	
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/stock/:stock/*action", func(c *gin.Context) {
+		stock := c.Param("stock")
+		action := c.Param("action")
+		if action == "add" {
+			e.newStockType(stock)
+		} else if action == "describe" {
+			price, err := e.describeStock(stock)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+			c.JSON(http.StatusOK, gin.H{"status": price})
+		}
+	})
+
+	r.POST("/buy/:stock", func(c *gin.Context) {
+		stock := c.Param("stock")
+		var json Order
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		_, err := e.buy(stock, json.Price, json.Volume)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "added buy order"})
+	})
+
+	r.POST("/sell/:stock", func(c *gin.Context) {
+		stock := c.Param("stock")
+		var json Order
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		err := e.addSell(stock, &Item{
+			price:  json.Price,
+			volume: json.Volume,
+		})
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "added sell order"})
+	})
+
+	err := r.Run()
+	if err != nil {
+		panic(err)
+	}
 }
